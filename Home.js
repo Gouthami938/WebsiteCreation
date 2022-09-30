1d0670aa32586bc3df7bc461c67ccdcd52a32c8c
Gouthami938 <70152870+Gouthami938@users.noreply.github.com>
2022-09-30 18:58:01 +0530

    homepage

---
+++ Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+    <div class="title">
+    <h1> <pre>                  FIND THE ONE     
+      
+      WHO YOU ARE LOOKING FOR</pre></h1>
+</div>)
+  };
+ 
+
+  export default Home;
+  
